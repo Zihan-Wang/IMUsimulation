fe37ba7a8929214576844cc0b5d59d5a35b05c76
James Di <jamesyubaidi@gmail.com>
2020-08-04 11:23:30 -0700

    Rename variables in converting script.

--- ov_msckf/src/kittipy/odometry_gt_converter.py
+++ ov_msckf/src/kittipy/odometry_gt_converter.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from numpy.linalg import inv
 from transformation_helpers import euler_to_quat
 import numpy as np
 import os
@@ -22,10 +23,14 @@ raw_data = pykitti.raw(raw_data_path, date, drive)
 
 output_path = os.path.join(odometry_path, "poses/" + sequence + "_converted.txt")
 
+
 # Transform the coordinate, such that the z axis point upwards. 
 P = np.array([[1, 0, 0, 0], [0, 0, 1, 0], [0, -1, 0, 0], [0, 0, 0, 1]])
 
-poses_camk_imu = [P.dot(T.dot(raw_data.calib.T_cam0_imu)) for T in odometry_data.poses]
+poses_w_imu = [P.dot(T.dot(raw_data.calib.T_cam0_imu)) for T in odometry_data.poses]
+# T_cam0_imu = raw_data.calib.T_cam0_imu
+# T_imu_cam0 = inv(T_cam0_imu)
+# poses_w_imu = [T_imu_cam0.dot(T.dot(T_cam0_imu)) for T in odometry_data.poses]
 
 
 # dataset.calib:      Calibration data are accessible as a named tuple
@@ -36,14 +41,13 @@ poses_camk_imu = [P.dot(T.dot(raw_data.calib.T_cam0_imu)) for T in odometry_data
 # dataset.rgb:        Generator to load RGB stereo pairs (cam2, cam3)
 # dataset.velo:       Generator to load velodyne scans as [x,y,z,reflectance]
 
-print("Shape of the ground-truth poses: " + str(len(poses_camk_imu)))
+print("Shape of the ground-truth poses: " + str(len(poses_w_imu)))
 print("\nShape of timestamps: " + str(len(odometry_data.timestamps)))
 
 print("=======================================")
 
-print("\nExample of ground-truth pose: " + str(poses_camk_imu[0]))
+print("\nExample of ground-truth pose: " + str(poses_w_imu[0]))
 print("\nExample of timestamps: " + str(odometry_data.timestamps[0:10]))
-
 
 # extract timestamps
 timestamps = np.array(map(lambda x: x.total_seconds(), odometry_data.timestamps)).reshape(-1, 1)
@@ -51,17 +55,17 @@ timestamps = np.array(map(lambda x: x.total_seconds(), odometry_data.timestamps)
 # print(timestamps.shape)
 
 # extra Rotations and Translations
-(R_w_cam0, p_w_cam0) = zip(*map(lambda pose: (pose[0:3, 0:3], pose[0:3, 3]), poses_camk_imu))
-print("\nExample of extracted rotation: " + str(R_w_cam0[0]))
-print("\nExample of extracted translation: " + str(p_w_cam0[0]))
+(R_w_imu, p_w_imu) = zip(*map(lambda pose: (pose[0:3, 0:3], pose[0:3, 3]), poses_w_imu))
+print("\nExample of extracted rotation: " + str(R_w_imu[0]))
+print("\nExample of extracted translation: " + str(p_w_imu[0]))
 
-q_w_cam0 = np.array(map(euler_to_quat, R_w_cam0))
-p_w_cam0 = np.array(p_w_cam0)
+q_w_imu = np.array(map(euler_to_quat, R_w_imu))
+p_w_imu = np.array(p_w_imu)
 
-qs, qv = q_w_cam0[:, 0].reshape(-1, 1), q_w_cam0[:, 1:] 
+qs, qv = q_w_imu[:, 0].reshape(-1, 1), q_w_imu[:, 1:] 
 
 # Construct dataset and output
-dataset = pd.DataFrame(np.hstack((timestamps, p_w_cam0, qv, qs))) # (timestamp(s) tx ty tz qx qy qz qw)
+dataset = pd.DataFrame(np.hstack((timestamps, p_w_imu, qv, qs))) # (timestamp(s) tx ty tz qx qy qz qw)
 dataset.to_csv(output_path, sep=' ', header=False, index=False)
  
 
